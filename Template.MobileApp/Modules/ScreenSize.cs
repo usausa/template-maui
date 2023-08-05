dd0fba7e654ecb0de70d8ff7dc7c95eb248461ed
usausa <machi.pon@gmail.com>
2023-08-05 22:13:49 +0900

    Update setting

---
+++ Template.MobileApp/Modules/ScreenSize.cs
@@ -0,0 +1,8 @@
+namespace Template.MobileApp.Modules;
+
+public static class ScreenSize
+{
+    public static double Height { get; } = DeviceDisplay.Current.MainDisplayInfo.Height / DeviceDisplay.Current.MainDisplayInfo.Density;
+
+    public static double Width { get; } = DeviceDisplay.Current.MainDisplayInfo.Width / DeviceDisplay.Current.MainDisplayInfo.Density;
+}
