6b6572a50fcf2a767074b0ac2396f88d0c857b04
usausa <machi.pon@gmail.com>
2024-02-06 13:49:37 +0900

    Update setting

--- Template.MobileApp/Log.cs
+++ Template.MobileApp/Log.cs
@@ -1,7 +1,6 @@
 namespace Template.MobileApp;
 
-#pragma warning disable SYSLIB1006
-public static partial class Log
+internal static partial class Log
 {
     [LoggerMessage(Level = LogLevel.Information, Message = "Application start.")]
     public static partial void InfoApplicationStart(this ILogger logger);
@@ -15,4 +14,3 @@ public static partial class Log
     [LoggerMessage(Level = LogLevel.Debug, Message = "Connectivity changed. profile=[{profile}], access=[{access}]")]
     public static partial void DebugConnectivityState(this ILogger logger, NetworkProfile profile, NetworkAccess access);
 }
-#pragma warning restore SYSLIB1006
