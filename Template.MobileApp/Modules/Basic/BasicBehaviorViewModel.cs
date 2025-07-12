117e04bb09bbd713ef3c35fe54ecacf9c8efe782
usausa <machi.pon@gmail.com>
2025-07-12 17:45:43 +0900

    Update baseline

--- Template.MobileApp/Modules/Basic/BasicBehaviorViewModel.cs
+++ Template.MobileApp/Modules/Basic/BasicBehaviorViewModel.cs
@@ -1,7 +1,24 @@
 namespace Template.MobileApp.Modules.Basic;
 
-public sealed class BasicBehaviorViewModel : AppViewModelBase
+public sealed partial class BasicBehaviorViewModel : AppViewModelBase
 {
+    public IObserveCommand FocusedCommand { get; }
+
+    public IObserveCommand UnfocusedCommand { get; }
+
+    [ObservableProperty]
+    public partial string Focused { get; set; } = string.Empty;
+
+    [ObservableProperty]
+    public partial string Unfocused { get; set; } = string.Empty;
+
+    public BasicBehaviorViewModel()
+    {
+        // [MEMO] Do not use individual focus control in real application.
+        FocusedCommand = MakeDelegateCommand<string>(x => Focused = x);
+        UnfocusedCommand = MakeDelegateCommand<string>(x => Unfocused = x);
+    }
+
     protected override Task OnNotifyBackAsync() => Navigator.ForwardAsync(ViewId.BasicMenu);
 
     protected override Task OnNotifyFunction1() => OnNotifyBackAsync();
--- Template.MobileApp/Modules/Basic/BasicConverterViewModel.cs
+++ Template.MobileApp/Modules/Basic/BasicConverterViewModel.cs
@@ -1,7 +1,28 @@
 namespace Template.MobileApp.Modules.Basic;
 
-public sealed class BasicConverterViewModel : AppViewModelBase
+public sealed partial class BasicConverterViewModel : AppViewModelBase
 {
+    [ObservableProperty]
+    public partial bool ReverseChecked { get; set; }
+
+    [ObservableProperty]
+    public partial bool MultiChecked1 { get; set; }
+    [ObservableProperty]
+    public partial bool MultiChecked2 { get; set; }
+
+    [ObservableProperty]
+    public partial bool ToChecked { get; set; }
+
+    [ObservableProperty]
+    public partial string Text { get; set; } = string.Empty;
+
+    public IObserveCommand SwitchTextCommand { get; }
+
+    public BasicConverterViewModel()
+    {
+        SwitchTextCommand = MakeDelegateCommand(() => Text = String.IsNullOrEmpty(Text) ? "Hello World" : string.Empty);
+    }
+
     protected override Task OnNotifyBackAsync() => Navigator.ForwardAsync(ViewId.BasicMenu);
 
     protected override Task OnNotifyFunction1() => OnNotifyBackAsync();
