968b396784e708c027bebafbb14e2380ff71a46a
usausa <machi.pon@gmail.com>
2026-09-03 21:32:11 +0900

    Update

--- Template.MobileApp/GeneratedFactory.cs
+++ Template.MobileApp/GeneratedFactory.cs
@@ -1,7 +1,3 @@
-// ライブラリ内部で登録される型は生成ファクトリの対象外になるため、明示的に生成させる
-// Types registered inside a library are out of scope for factory generation, so they are requested explicitly.
-// provider.DescribeRuntimeFallbacks() の出力を貼り付けたもの
-// Pasted from the output of provider.DescribeRuntimeFallbacks().
 #pragma warning disable IDE0001
 [assembly: global::BunnyTail.DependencyInjection.GenerateComponentFactory(typeof(global::Shiny.AndroidPlatform))]
 [assembly: global::BunnyTail.DependencyInjection.GenerateComponentFactory(typeof(global::Shiny.BluetoothLE.BleManager))]
